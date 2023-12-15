e060888c07f4541502675919b1e4e401abee1ee0
GianZapata <giancarlozapata13@gmail.com>
2023-12-14 23:16:54 -0600

    Sidebar

--- src/features/presentation/components/ui/sidebar/Sidebar.tsx
+++ src/features/presentation/components/ui/sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@
 import clsx from "clsx"
 import Link from "next/link"
 
-import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline } from "react-icons/io5"
+import { IoCloseOutline, IoLogInOutline, IoLogOutOutline, IoPeopleOutline, IoPersonOutline, IoSearchOutline, IoShirtOutline, IoTicketOutline, IoApps  } from "react-icons/io5"
 
 import { Page } from "@/config/enums/page.enum"
 import { useUIStore } from "@/features/presentation/store/ui/ui.store"
@@ -16,7 +16,7 @@ export const Sidebar = () => {
    return (
       <div>
 
-         {/* Backogrund black */}
+         {/* Background black */}
          { isSideMenuOpen && (
             <div
                className="fixed top-0 left-0 w-screen h-screen z-10 bg-black opacity-30"
@@ -36,7 +36,7 @@ export const Sidebar = () => {
          <nav  
             className={ 
                clsx(
-                  "fixed p-5 right-0 top-0 w-[500px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
+                  "fixed right-0 top-0 w-[250px] h-screen bg-white z-20 shadow-2xl transform transition-all duration-300",
                   {
                      "translate-x-full": !isSideMenuOpen
                   }
@@ -44,12 +44,12 @@ export const Sidebar = () => {
             }
          >
             <IoCloseOutline
-               size={30}
-               className="absolute top-5 right-5 cursor-pointer"
+               size={25}
+               className="absolute top-5 right-5 text-gray-500 hover:text-gray-800 cursor-pointer transition-all duration-300 "
                onClick={ () => closeMenu() }
             />
 
-            <div className="relative mt-14">
+            <div className="relative mt-14 mb-3">
                <IoSearchOutline size={ 20 } className="absolute top-2 left-2" />
                <input type="text"
                   className="w-full bg-gray-50 rounded pl-10 py-1 border-b-2 text-xl border-gray-200 focus:outline-none focus:border-blue-500"
@@ -57,41 +57,46 @@ export const Sidebar = () => {
                />
             </div>
 
-            <Link href={Page.Home} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoPersonOutline size={30} />
-               <span className="ml-3 text-xl">Perfil</span>
+            <Link href={Page.Home} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoPersonOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Perfil</span>
             </Link>
 
-            <Link href={Page.Orders} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoTicketOutline size={30} />
-               <span className="ml-3 text-xl">Ordenes</span>
+            <Link href={Page.Orders} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoTicketOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Ordenes</span>
             </Link>
 
-            <Link href={Page.AuthLogin} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoLogInOutline size={30} />
-               <span className="ml-3 text-xl">Ingresar</span>
+            <Link href={Page.AuthLogin} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoLogInOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Ingresar</span>
             </Link>
 
-            <Link href={Page.AuthLogin} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoLogOutOutline size={30} />
-               <span className="ml-3 text-xl">Salir</span>
+            <Link href={Page.AuthLogin} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoLogOutOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Salir</span>
             </Link>
 
-            <div className="w-full h-px bg-gray-200 my-10"/>
+            <div className="w-full h-px bg-gray-200 my-3"/>
 
-            <Link href={Page.Products} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoShirtOutline size={30} />
-               <span className="ml-3 text-xl">Products</span>
+            <Link href={Page.Products} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoApps size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Dashboard</span>
             </Link>
 
-            <Link href={Page.Orders} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoTicketOutline size={30} />
-               <span className="ml-3 text-xl">Ordenes</span>
+            <Link href={Page.Products} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoShirtOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Products</span>
             </Link>
 
-            <Link href={Page.AdminUsers} className="flex items-center mt-10 p-2 hover:bg-gray-100 rounded transition-all">
-               <IoPeopleOutline size={30} />
-               <span className="ml-3 text-xl">Usuarios</span>
+            <Link href={Page.Orders} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoTicketOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Ordenes</span>
+            </Link>
+
+            <Link href={Page.AdminUsers} className="flex items-center py-3 px-4 hover:bg-gray-50 rounded transition-all">
+               <IoPeopleOutline size={25} className={'text-gray-700'} />
+               <span className="ml-3 text-xl text-gray-700">Usuarios</span>
             </Link>
 
          </nav>
